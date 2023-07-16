607219e28f32cfc84b9afb6b252f0de5c526f72c
lars kvn <kvnyarbis@gmail.com>
2023-07-15 20:48:54 -0500

    cors update

---
+++ src/main/java/com/filmplanet/WebConfig/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.filmplanet.WebConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://smartheal.vercel.app","http://localhost:3000","https://smarth-api-gateway.azurewebsites.net/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}
